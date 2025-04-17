c1847c57ae255a7df01a090a2aea9811c58775d6
mohamed0yaser <mohamedyaser2023@gmail.com>
2025-04-17 20:40:49 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:meeting_application/screens/login_screen.dart';
+import 'package:meeting_application/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: backgroundColor
+        ),
+      home:LogInPage(),
+    );
+  }
+}
